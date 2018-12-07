65cc781d61321afab7af24114f5aab0c0ed2e209
steave6 <todayonewintomorrowtwo@yahoo.co.jp>
2018-12-07 23:38:04 +0900

    added docker integration

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+#! /usr/bin/env node
 var liveServer = require("live-server");
 
 var params = {
-	port: 8181, // Set the server port. Defaults to 8080.
-	host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	port: 8000, // Set the server port. Defaults to 8080.
+	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
 	root: "./public", // Set root directory that's being served. Defaults to cwd.
-	open: true, // When false, it won't load your browser by default.
+	open: false, // When false, it won't load your browser by default.
 	ignore: 'scss,my/templates', // comma-separated string for paths to ignore
 	// file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
 	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
